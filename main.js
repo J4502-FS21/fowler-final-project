cd87617630280bc8eed15024d4e8ac1ba9f0b612
KenedeeF <kmfhmc@umsystem.edu>
2021-12-12 19:34:56 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var menuNav = document.getElementById("menu_nav");
+
+var menuTrigger = document.getElementById('menu_trigger');
+
+function menuToggle() {
+  console.log(menuNav.style.display);
+  if (menuNav.style.display === "flex") {
+    menuNav.style.display = "none";
+  } else {
+    menuNav.style.display = "flex";
+  }
+  console.log("The menu has been toggled!");
+}
+
+function checkWindowSize() {
+  if (window.innerWidth >= 600) {
+    menuNav.style.display = "flex";
+  }
+}
+
+menuTrigger.onclick = menuToggle;
+
+window.onresize = checkWindowSize;
