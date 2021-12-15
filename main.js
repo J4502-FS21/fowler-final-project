5936036d3f36f1113bc26d124dd6ba5f4b486c35
KenedeeF <kmfhmc@umsystem.edu>
2021-12-14 19:58:47 -0600

    updated

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ menuTrigger.onclick = menuToggle;
 
 window.onresize = checkWindowSize;
 
-var slideIndex = 2;
+var slideIndex = 1;
 showSlides(slideIndex)
 
 function plusSlides(n) {
@@ -37,15 +37,14 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
+//  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+  /*for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
-  }
+  }*/
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+//  dots[slideIndex-1].className += " active";
 }
